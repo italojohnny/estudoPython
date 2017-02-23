78d13eedd2218dc4aa0bcb7d52669a1c0da170e2
italojohnny <italojohnnydosanjos@gmail.com>
2017-02-23 00:51:57 -0300

    criacao de exemplo de servico linux

---
+++ tmp/servico_linux/program.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from random import randint
+from time import sleep
+
+filename = "/tmp/mylog.dat"
+while True:
+    value1 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    value2 = "%0.3d" % randint(0,999)
+    
+    try:
+        filelog = open(filename, 'a')
+        filelog.write("%s -- %s\n" % (value1, value2))
+
+    except:
+        filelog = open(filename, 'w')
+        filelog.write("%s -- %s\n" % (value1, value2))
+
+    finally:
+        filelog.close()
+
+    sleep(1)
